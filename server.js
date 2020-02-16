0a8f85d477a5aaeb6ab735a733cfe3f27fa48838
ankush bansal <ankushbansal1221@gmail.com>
2020-02-16 16:17:15 +0530

    add changes of https to http

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(express.static(__dirname + '/dist/apticlg'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname + '/dist/apticlg/index.html'));
 });
-
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
+  });
 
 app.listen(process.env.PORT || 4600);
