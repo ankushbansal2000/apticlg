2059af1b2adb816f4b1d2941e9a6d08cd160ae25
ankush bansal <ankushbansal1221@gmail.com>
2020-02-16 15:19:16 +0530

    package file changes for deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/apticlg'));
+// Start the app by listening on the default
+
+// Heroku port
+app.get('/', function(req,res) {
+res.sendFile(path.join(__dirname + '/dist/apticlg/index.html'));
+});
+
+
+app.listen(process.env.PORT || 4600);
