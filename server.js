327cf2112f52281698be84ddc7509de01cedce7c
ankush bansal <ankushbansal1221@gmail.com>
2020-05-05 15:08:26 +0530

    path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+var path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist/apticlg'));
